070d2bcb65c44c24918b5fd7e69caf5abf6ca287
M. Soban Amir <62153801+LoRdSoban@users.noreply.github.com>
2022-08-28 16:43:09 +0500

    1537A

---
+++ Solutions/CodeForces/1537A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define forn(n) for(int i=0;i<n;i++)
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using namespace std;
+
+
+int main()
+{
+    IOS;
+    int T;
+
+    int n, sum, a,ans;
+
+    cin >> T;
+    while (T--)
+    {
+        sum =0;
+        cin >> n;
+        forn(i)
+        {
+            cin >> a;
+            sum += a;
+        }
+
+        
+    }
+
+    return 0;
+}
+
+
+`
